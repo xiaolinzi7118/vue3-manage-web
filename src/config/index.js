547e6fd0f4ce24a468f05fd6f311fcf949675d68
xiaolinzi7118 <1106147743@qq.com>
2021-07-11 23:06:45 +0800

    feat:环境变量配置

---
+++ src/config/index.js
@@ -0,0 +1,23 @@
+/**
+ * 环境变量配置
+ */
+const env = import.meta.env.MODE || 'prod';
+const EnvConfig = {
+    development: {
+        baseApi: '/api',
+        mockApi: 'https://www.fastmock.site/mock/2b6d7acc62fddc27a5bb96138fbd0d91/api'
+    },
+    test: {
+        baseApi: '//test.linzi.com/api',
+        mockApi: 'https://www.fastmock.site/mock/2b6d7acc62fddc27a5bb96138fbd0d91/api'
+    },
+    prod: {
+        baseApi: '//linzi.com/api',
+        mockApi: 'https://www.fastmock.site/mock/2b6d7acc62fddc27a5bb96138fbd0d91/api'
+    }
+}
+export default {
+    env,
+    mock: 'true',
+    ...EnvConfig[env]
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/lib/theme-chalk/index.css';
+import axios from 'axios';
+import config from './config'
 
 const app = createApp(App);
+console.log(config)
+axios.get(config.mockApi + '/login').then(res => {
+    console.log(res)
+})
 app.use(router).use(ElementPlus).mount('#app')
