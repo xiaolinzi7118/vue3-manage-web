b3febf8cceff1fac17cca2b45f2d6cf5fec2cf81
xiaolinzi7118 <1106147743@qq.com>
2023-01-30 18:05:12 +0800

    feat: 插件升级

--- src/components/TreeMenu.vue
+++ src/components/TreeMenu.vue
@@ -1,6 +1,6 @@
 <template>
   <template v-for="menu in userMenu">
-    <el-submenu
+    <el-sub-menu
       v-if="
         menu.children &&
         menu.children.length > 0 &&
@@ -14,7 +14,7 @@
         <span>{{ menu.menuName }}</span>
       </template>
       <tree-menu :userMenu="menu.children" />
-    </el-submenu>
+    </el-sub-menu>
     <el-menu-item
       v-else-if="menu.menuType === '1'"
       :index="menu.path"
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css';
+import 'element-plus/dist/index.css';
 import request from './utils/request'
 import storage from './utils/storage'
 import api from './api'
